1609997fd59ce2c041c2ca6bd69d30346fd222d6
Joao Patricio <ijpatricio@gmail.com>
2024-05-08 09:43:03 +0100

    Add Coordinates validator

---
+++ app/CoordinatesValidator.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App;
+
+class CoordinatesValidator
+{
+    /**
+     * Check if a given point is inside the state of Rio Grande do Sul
+     */
+    public function isPointInsideRioGrandeDoSul(float $latitude, float $longitude): bool
+    {
+        return $this->isPointInPolygon(
+            latitude: $latitude,
+            longitude: $longitude,
+            polygon: $this->coordinatesRioGrandeDoSul()
+        );
+    }
+
+    /**
+     * Calculate if a given point is inside a polygon
+     */
+    private function isPointInPolygon(float $latitude, float $longitude, array $polygon): bool
+    {
+        $inside = false;
+        $x = $latitude;
+        $y = $longitude;
+
+        for ($i = 0, $j = count($polygon) - 1; $i < count($polygon); $j = $i++) {
+            $xi = $polygon[$i][0];
+            $yi = $polygon[$i][1];
+            $xj = $polygon[$j][0];
+            $yj = $polygon[$j][1];
+
+            $intersect = (($yi > $y) != ($yj > $y)) && ($x < ($xj - $xi) * ($y - $yi) / ($yj - $yi) + $xi);
+            if ($intersect) {
+                $inside = !$inside;
+            }
+        }
+
+        return $inside;
+    }
+
+    /**
+     * The points of the polygon that defines the state of Rio Grande do Sul
+     *
+     * @return array[<float>, <float>] - Latitude and Longitude
+     */
+    private function coordinatesRioGrandeDoSul(): array
+    {
+        return [
+            [-53.0014372, -34.0164553],
+            [-52.5619841, -33.3215637],
+            [-51.9906950, -32.4172837],
+            [-50.6537533, -31.3353109],
+            [-49.7604704, -29.6882764],
+            [-49.5990658, -29.0566199],
+            [-49.6396208, -28.4592594],
+            [-50.4965544, -28.2852600],
+            [-51.1447477, -27.6253685],
+            [-52.1554899, -27.0789208],
+            [-53.8034391, -27.0397859],
+            [-54.5944548, -27.3915068],
+            [-55.9347868, -28.3722954],
+            [-57.1086287, -29.2242085],
+            [-57.8392196, -30.2496885],
+            [-57.3849821, -30.4867727],
+            [-56.5643978, -30.5248569],
+            [-55.9347868, -31.2881599],
+            [-55.5282927, -31.1660299],
+            [-54.4845915, -31.7844358],
+            [-53.3969450, -32.7967266],
+            [-53.7265348, -33.7154158],
+            [-53.0014372, -34.0164553],
+        ];
+    }
+
+}
--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,19 +1,62 @@
 <?php
 
-namespace Tests\Feature;
+use App\CoordinatesValidator;
+use Illuminate\Support\Facades\File;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
-use Tests\TestCase;
+it('returns a successful response', function () {
+    $response = $this->get('/');
 
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     */
-    public function test_the_application_returns_a_successful_response(): void
-    {
-        $response = $this->get('/');
+    $response->assertStatus(200);
+});
 
-        $response->assertStatus(200);
+test('Coordiantes validator', function () {
+
+    $validator = new CoordinatesValidator;
+
+    $northAmericaCapitals = File::json(base_path('tests/fixtures/north-america-capitals.json'));
+    $northAmericaCapitalsCoordiantes = data_get($northAmericaCapitals, '*.geometry.coordinates');
+
+    // North America capitals are outside of Rio Grande do Sul
+    foreach ($northAmericaCapitalsCoordiantes as $coordinates) {
+        $latitude = $coordinates[1];
+        $longitude = $coordinates[0];
+
+        $res = $validator->isPointInsideRioGrandeDoSul($latitude, $longitude);
+
+        expect($res)->toBeFalse();
     }
-}
+
+    // Points inside of Rio Grande do Sul
+    $pointsInside = [
+        [-55.2353954, -29.4169465],
+        [-54.4224072, -28.6484905],
+        [-52.8403759, -28.3008289],
+        [-51.6758251, -28.7641229],
+        [-51.6648388, -29.7799482],
+        [-52.9612255, -30.6627960],
+        [-54.5542431, -30.5871644],
+        [-53.3237743, -29.6845482],
+        [-55.4661083, -30.3125053],
+        [-55.3452587, -28.8122653],
+        [-52.8953075, -27.9229097],
+        [-50.7969189, -28.7063227],
+        [-51.9834423, -30.5304019],
+        [-53.4226513, -31.4345952],
+        [-52.4778271, -29.8180828],
+        [-53.5654736, -29.2445434],
+        [-56.1802197, -29.6559105],
+        [-55.9604931, -30.4167774],
+        [-54.2356396, -31.1153408],
+    ];
+
+    foreach ($pointsInside as $point) {
+        $latitude = $point[0];
+        $longitude = $point[1];
+
+        $res = $validator->isPointInsideRioGrandeDoSul($latitude, $longitude);
+
+        expect($res)->toBeTrue();
+    }
+
+
+});
